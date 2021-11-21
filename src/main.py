5897b0a53aea961464203999586c02fa916f0a11
matheus <matheusvictor@ufpi.edu.br>
2021-11-21 10:20:32 -0300

    Categorias acrescentadas: idade, altura e email

--- src/main.py
+++ src/main.py
@@ -27,10 +27,43 @@ for i in range(10):
 '''
 
 
-
+'''
 print('Sorteio de imagens com cores diferentes: ')
 for i in range(10):
 	imagem = diz.imagem()
 	imagem.save("imagem"+str(i)+".png")
 	os.system("xdg-open imagem"+str(i)+".png")
+'''
+
+
+'''
+print('Sorteio de idades:')
+for i in range(3):
+	print(diz.idade(5, minimo=18))
+	print(diz.idade(crianca=True),'anos.')
+	print(diz.idade(adolescente=True),'anos.')
+	print(diz.idade(adulto=True),'anos.')
+	print(diz.idade(idoso=True),'anos.')
+	print('-' * 15)
+'''
+
+
+'''
+print('Sorteio de alturas: ')
+for i in range(3):
+	print(diz.altura(),'m')
+	print(diz.altura(minimo=1.5),'m')
+	print(diz.altura(3))
+'''
+
+
+
+print('Sorteio de emails:')
+for i in range(3):
+	print(diz.email())
+	print(diz.email(nome='jorge'))
+	print(diz.email(3))
+	print('-'*15)
+
+
 
